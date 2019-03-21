b30ca98d6480f1f201fc4a0e9ac1627015446644
Brian Cavalier <brian@briancavalier.com>
2019-03-21 16:20:21 -0400

    Update example naming and comments

--- examples/echo-console.ts
+++ examples/echo-console.ts
@@ -1,20 +1,16 @@
-import { Cancel, chain, Env, map, runPure, uncancelable, use } from '../src'
+import { Cancel, chain, Env, forever, map, runPure, use, withEnv } from '../src'
 import { EOL } from 'os'
 import { createInterface } from 'readline'
 
-// Helper to loop a computation forever
-const forever = <R, A>(e: Env<R, A>): Env<R, never> =>
-  chain(() => forever(e), e)
-
-// Print effect and constructor
+// Print capability
 type Print = {
-  print (s: string, k: (r: void) => void): Cancel
+  print (s: string): void
 }
 
 const print = (s: string): Env<Print, void> =>
-  ({ print }, k) => print(s, k)
+  withEnv(({ print }) => print(s))
 
-// Read effect and constructor
+// Read capability
 type Read = {
   read (k: (r: string) => void): Cancel
 }
@@ -23,7 +19,7 @@ const read: Env<Read, string> =
   ({ read }, k) => read(k)
 
 // Helper to append newlines
-const addEol = (s: string): string => `${s}${EOL}`
+const addEol = (s: string): string => s + EOL
 
 // Effectful computation that prints a prompt, reads
 // user input and prints it.
@@ -31,23 +27,21 @@ const echo: Env<Print & Read, void> =
   chain(() => chain(print, map(addEol, read)), print('> '))
 
 // To run echo, we need to provide implementations of
-// Read and Print
+// Print and Read
 
 // We'll use node's readline to implement Read
 const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout
+  input: process.stdin
 })
 
 // Concrete Print and Read implementations
-const resources: Print & Read = {
-  print: (s, k): Cancel =>
-    uncancelable(k(void process.stdout.write(s))),
+const capabilities: Print & Read = {
+  print: s => void process.stdout.write(s),
   read: k => {
     readline.once('line', k)
     return () => readline.removeListener('line', k)
   }
 }
 
-// Loop echo forever using the effect usingrs
-runPure(use(resources, forever(echo)))
+// Loop echo forever using the Print and Read capabilities
+runPure(use(capabilities, forever(echo)))
