4077eb6a80dbe8846de710ae68a247fd9371fc25
Brian Cavalier <brian@briancavalier.com>
2019-09-04 08:52:12 -0400

    Add error effect

---
+++ src/error.ts
@@ -0,0 +1,11 @@
+import { Cancel, Env, Subtract, use } from './env';
+
+export type Fail<E> = {
+    catchError <A>(e: E, k: (a: A) => Cancel): Cancel
+}
+
+export const throwError = <E, A> (e: E): Env<Fail<E>, A> =>
+    (r, k) => r.catchError(e, k)
+
+export const catchError = <E, R extends Fail<E>, A> (f: (e: E) => A, e: Env<R, A>): Env<Subtract<R, Fail<E>>, A> =>
+    use({ catchError: (e: E, k: (a: A) => Cancel): Cancel => k(f(e)) }, e)
